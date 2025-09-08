b093dcb1b51defb9ef96af278f23f91801c377d1
Matthew Garcia <matt.e.garcia@gmail.com>
2025-09-08 15:55:35 -0500

    moved InputClimateParms class to its own file

--- src/IInputClimateParms.cs
+++ src/IInputClimateParms.cs
@@ -1,44 +1,12 @@
 //  Authors:  Caren Dymond, Sarah Beukema
 
-using Landis.Library.Succession;
-using Landis.Core;
-using Landis.Utilities;
-using Landis.Library.Parameters;
-using System.Collections.Generic;
-using System.Diagnostics;
-
 namespace Landis.Extension.Succession.ForC
 {
     /// <summary>
-    /// The parameters for ForC Cliamte initialization.
+    /// The parameters for ForC climate initialization.
     /// </summary>
     public interface IInputClimateParms
     {
         Landis.Library.Parameters.Ecoregions.AuxParm<ITimeCollection<IClimateAnnual>> ClimateAnnualCollection { get; }
-
     }
-
-        /// </summary>
-    public class InputClimateParms
-        : IInputClimateParms
-    {
-        private Landis.Library.Parameters.Ecoregions.AuxParm<ITimeCollection<IClimateAnnual>> m_ClimateAnnualCollection; 
-        private IEcoregionDataset m_dsEcoregion;
-
-        public InputClimateParms()
-        {
-            this.m_dsEcoregion = PlugIn.ModelCore.Ecoregions;
-            this.m_ClimateAnnualCollection = new Landis.Library.Parameters.Ecoregions.AuxParm<ITimeCollection<IClimateAnnual>>(m_dsEcoregion);
-            foreach (IEcoregion ecoregion in m_dsEcoregion)
-            {
-                this.m_ClimateAnnualCollection[ecoregion] = new TimeCollection<IClimateAnnual>();
-            }
-
-         }
-        public Landis.Library.Parameters.Ecoregions.AuxParm<ITimeCollection<IClimateAnnual>> ClimateAnnualCollection { get { return m_ClimateAnnualCollection; } }
-
-        //---------------------------------------------------------------------
-
-    }
-
 }
