7368ed285cf127c8c51346374b0534e0d692a116
Matthew Garcia <matt.e.garcia@gmail.com>
2025-09-15 11:48:27 -0500

    accidentally left some constants private, now public

--- src/Constants.cs
+++ src/Constants.cs
@@ -8,11 +8,11 @@ namespace Landis.Extension.Succession.ForC
         public const int FIREINTENSITYCOUNT = 5;
         public const int NUMSNAGS = 1000;
         public const int NUMBIOMASSCOMPONENTS = 6;  // ComponentType.FINEROOT + 1, The total number of biomass components.
-        private const double BIOTOC = 0.5;
+        public const double BIOTOC = 0.5;
         public const int NUMSOILPOOLS = 10; // SoilPoolType.BLACKCARBON + 1;
-        private const int NUMSNAGPOOLS = 2; // Snags.SnagType.OTHERSNAG + 1 i.e., stem and branches snag pool
-        private const double FINEROOTSABOVERATIO = 0.5;
-        private const double COARSEROOTABOVERATIO = 0.5;
-        private const int NUMDISTURBANCES = 9;  // note, if add more dists, then increase this
+        public const int NUMSNAGPOOLS = 2; // Snags.SnagType.OTHERSNAG + 1 i.e., stem and branches snag pool
+        public const double FINEROOTSABOVERATIO = 0.5;
+        public const double COARSEROOTABOVERATIO = 0.5;
+        public const int NUMDISTURBANCES = 9;  // note, if add more dists, then increase this
     }
 }
